3b9beb1a414961f9efc0571bfde80c3b2ca67536
Sriharsha Damarla <32672504+sritar99@users.noreply.github.com>
2018-10-18 18:32:48 +0530

    Add files via upload

---
+++ Shapes.java
@@ -0,0 +1,65 @@
+public class Shapes {
+    public static void main (String [] args) {
+        System.out.println ("SQUARE:\n");
+         
+        System.out.print ("********* \n");
+        System.out.print ("*       * \n");
+        System.out.print ("*       * \n");
+        System.out.print ("*       * \n");
+        System.out.print ("*       * \n");
+        System.out.print ("*       * \n");
+        System.out.print ("*       * \n");
+        System.out.print ("*       * \n");
+        System.out.print ("********* \n");
+
+        System.out.print ("\n\n\n");
+
+        System.out.println ("ELLIPSE:\n");
+
+
+        System.out.print ("   *** \n");
+        System.out.print (" *     *\n");
+        System.out.print ("*       *\n");
+        System.out.print ("*       *\n");
+        System.out.print ("*       *\n");
+        System.out.print ("*       *\n");
+        System.out.print ("*       *\n");
+        System.out.print (" *     *\n");
+        System.out.print ("   *** \n");
+
+
+        System.out.print ("\n\n\n");
+
+        System.out.println ("ARROW:\n");
+
+
+
+        System.out.print ("    * \n");
+        System.out.print ("   *** \n");
+        System.out.print ("  *****\n");
+        System.out.print ("    *  \n");
+        System.out.print ("    * \n");
+        System.out.print ("    *  \n");
+        System.out.print ("    *  \n");
+        System.out.print ("    *  \n");
+        System.out.print ("    * \n");
+
+
+        System.out.print ("\n\n\n");
+
+        System.out.println ("DIAMOND:\n");
+
+
+
+        System.out.print ("    * \n");
+        System.out.print ("   * * \n");
+        System.out.print ("  *   * \n");
+        System.out.print (" *     * \n");
+        System.out.print ("*       * \n");
+        System.out.print (" *     * \n");
+        System.out.print ("  *   * \n");
+        System.out.print ("   * * \n");
+        System.out.print ("    * \n");
+
+    }
+}
